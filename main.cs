63f80745efb7c2a6db9cb8ce43b12293a6ceb3ea
KateParshina1533 <64014128+KateParshina1533@users.noreply.github.com>
2020-04-20 18:45:31 +0400

    Create main.cs
    create.main.cs

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class MainClass
+{
+  public static void Main (string[] args)
+    {
+        Console.WriteLine("Hello World");
+    int a = 4, b = 3;
+    int c = a + b;
+        Console.WriteLine(c);
+    
+  }
+}
